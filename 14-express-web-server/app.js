6ed6a53d5ed8034cb1631e087bdc562a6e1d0e90
yoka 🥑 <101960701+AllPooCat@users.noreply.github.com>
2025-05-22 18:21:40 +0800

    Perubahan kedua
    routing menggunakan expressJs
    mengambil product id dari url

--- 14-express-web-server/app.js
+++ 14-express-web-server/app.js
@@ -1,52 +1,93 @@
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+//================PROGRAM SEBELUM EXPRESSJS==========================
+// const http = require('http');
+// const fs = require('fs');
+// const port = 3000;
 
-const renderHTML = (path, res) => {
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            res.write(404);
-            res.write('Error file not found');
-        } else {
-            res.write(data);
-        };
-        res.end();
-    });
-};
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if (err) {
+//             res.write(404);
+//             res.write('Error file not found');
+//         } else {
+//             res.write(data);
+//         };
+//         res.end();
+//     });
+// };
 
 
-//membuat server
-http.createServer((req, res) => {
-    //agar menampilkan konten html
-    res.writeHead(200, {
-        'content-type' : 'text/html',
-    });
+// //membuat server
+// http.createServer((req, res) => {
+//     //agar menampilkan konten html
+//     res.writeHead(200, {
+//         'content-type' : 'text/html',
+//     });
 
-    const url = req.url;
+//     const url = req.url;
 
-    //routing
-    switch (url) {
-        case '/about':
-            renderHTML('./about.html',res);
-            break;
-        case '/contact':
-            renderHTML('./contact.html',res);
-            break;
-        default:
-            renderHTML('./index.html',res);
-            break;
-    };
+//     //routing
+//     switch (url) {
+//         case '/about':
+//             renderHTML('./about.html',res);
+//             break;
+//         case '/contact':
+//             renderHTML('./contact.html',res);
+//             break;
+//         default:
+//             renderHTML('./index.html',res);
+//             break;
+//     };
 
-    // if (url === '/about') {
-    //     renderHTML('./about.html', res);
-    // } else if (url === '/contact') {
-    //     renderHTML('./contact.html', res);
-    // } else {
-    //     renderHTML('./index.html', res);
-    // };
+//     // if (url === '/about') {
+//     //     renderHTML('./about.html', res);
+//     // } else if (url === '/contact') {
+//     //     renderHTML('./contact.html', res);
+//     // } else {
+//     //     renderHTML('./index.html', res);
+//     // };
 
     
+// })
+// .listen(port, () =>{
+//     console.log(`Server is listening on port ${port}..`);
+// });
+//================PROGRAM SEBELUM EXPRESSJS==========================
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+    // res.json({
+    //     nama: 'Yoka',
+    //     email: 'yoka@gmail.com',
+    //     noHP: '08123456789',
+    // });
+    res.sendFile('./index.html',{root: __dirname});
+    //{root: __dirname} artinya berada di direktori yang sama
 })
-.listen(port, () =>{
-    console.log(`Server is listening on port ${port}..`);
-});
+
+app.get('/about', (req, res) => {
+//   res.send('Ini adalah halaman about yang akan keren!')
+    res.sendFile('./about.html',{root: __dirname});
+})
+
+app.get('/contact', (req, res) => {
+//   res.send('Ini adalah halaman contact yang akan keren!')
+    res.sendFile('./contact.html',{root: __dirname});
+})
+
+app.get('/product/:id', (req, res) => {
+    res.send('Product ID : '+ req.params.id);
+})
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>404</h1>');
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
